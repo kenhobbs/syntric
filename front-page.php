ad7ed760c59ed232f22f1922b7809cfd0ec390e9
Ken Hobbs <ken@syntric.com>
2018-02-28 13:24:55 -0800

    Commit and deploy a weeks worth of changes

--- front-page.php
+++ front-page.php
@@ -1,31 +1,8 @@
 <?php
 	get_header();
-	if ( syn_remove_whitespace() ) {
-		$lb  = '';
-		$tab = '';
-	} else {
-		$lb  = "\n";
-		$tab = "\t";
-	}
-	echo '<div id="front-page-wrapper" class="content-wrapper ' . get_post_type() . '-wrapper">' . $lb;
-	echo '<div class="' . esc_html( get_theme_mod( 'syntric_container_type' ) ) . '">' . $lb;
-	echo '<div class="row">' . $lb;
 	syn_sidebar( 'main', 'left' );
-	echo '<main id="content" class="col content-area content">' . $lb;
 	syn_sidebar( 'main', 'top' );
-	/*if ( have_posts() ) :
-		while( have_posts() ) : the_post();
-			if ( syn_has_content( the_content() ) ) :
-				echo '<article ' . post_class() . ' id="post-' . the_ID() . '">' . $lb;
-				the_content();
-				echo '</article>' . $lb;
-			endif;
-		endwhile;
-	endif;*/
 	syn_sidebar( 'main', 'bottom' );
-	echo '</main>' . $lb;
 	syn_sidebar( 'main', 'right' );
-	echo '</div>' . $lb;
-	echo '</div>' . $lb;
-	echo '</div>' . $lb;
 	get_footer();
+?>
